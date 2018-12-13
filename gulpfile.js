68f7d0fd49df9382fc902a6fb31711a520204783
Yury <yuryspb@gmail.com>
2018-12-13 18:41:08 +0300

    feat: add markup and styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
 
-gulp.task("sass", function() {
+gulp.task("style", function() {
   gulp
     .src("sass/style.scss")
+    .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest("css"));
+    .pipe(postcss([autoprefixer({ browsers: ["last 2 versions"] })]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
 });
